d37b057e0c1a93df05c30e5123f5e918b5860084
Sms_Gamer <creepercannyon@gmail.com>
2019-08-06 20:16:39 -0700

    Added logs and better /cv command.

--- src/uwu/smsgamer/uwup/commands/Commands.java
+++ src/uwu/smsgamer/uwup/commands/Commands.java
@@ -1,5 +1,12 @@
 package uwu.smsgamer.uwup.commands;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -18,6 +25,10 @@ public class Commands implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+
 		if (cmd.getName().equalsIgnoreCase("pu") || cmd.getName().equalsIgnoreCase("punish")) {
 			if (sender.hasPermission("uwu.punish.use")) {
 				if (args.length <= 2) {
@@ -42,6 +53,11 @@ public class Commands implements CommandExecutor {
 							ConfigManager.instance.getPlayers().set("Punishments." + num + ".Level." + p.getUniqueId(),
 									ConfigManager.instance.getPlayers()
 											.getInt(("Punishments." + num + ".Level." + p.getUniqueId())) + 1);
+
+							// set log
+
+							logToFile(dtf.format(now) + " | " + sender.getName() + " punished " + p.getName() + " for "
+									+ args[2] + ".");
 
 							// send message
 
@@ -113,8 +129,7 @@ public class Commands implements CommandExecutor {
 							return true;
 						}
 					}
-					sender.sendMessage(ChatUtils.colorize(
-							ChatUtils.phReplace(Vars.no_type, args[0], args[1], 0)));
+					sender.sendMessage(ChatUtils.colorize(ChatUtils.phReplace(Vars.no_type, args[0], args[1], 0)));
 					return true;
 				} else {
 					sender.sendMessage(ChatUtils.colorize(ChatUtils.phReplace(Vars.no_player, args[0], args[1], 0)));
@@ -126,22 +141,27 @@ public class Commands implements CommandExecutor {
 
 		if (cmd.getName().equalsIgnoreCase("cv") || cmd.getName().equalsIgnoreCase("checkvl")
 				|| cmd.getName().equalsIgnoreCase("checkviolations")) {
-			if (sender.hasPermission("uwu.forgive.use")) {
-				if (args.length <= 1) {
-					sender.sendMessage(ChatColor.RED + "UwU >> Usage: /checkviolations <player> <reason>");
+			if (sender.hasPermission("uwu.checkvl.use")) {
+				if (args.length == 0) {
+					sender.sendMessage(ChatColor.RED + "UwU >> Usage: /checkviolations <player> [reason]");
 					return true;
 				}
 				if (Bukkit.getPlayer(args[0]) != null) {
 					Player p = Bukkit.getPlayer(args[0]);
 					for (String num : Main.instance.getConfig().getConfigurationSection("types").getKeys(false)) {
-						if (Main.instance.getConfig().getStringList("types." + num + ".alias").contains(args[1])) {
-							sender.sendMessage(
-									ChatUtils.phReplace(Vars.check_vl, p.getName(), num, ConfigManager.instance
-											.getPlayers().getInt("Punishments." + num + ".Level." + p.getUniqueId())));
-							return true;
+						if (args.length != 1) {
+							if (Main.instance.getConfig().getStringList("types." + num + ".alias").contains(args[1])) {
+								sender.sendMessage(ChatUtils.phReplace(Vars.check_vl, p.getName(), num,
+										ConfigManager.instance.getPlayers()
+												.getInt("Punishments." + num + ".Level." + p.getUniqueId())));
+								return true;
+							}
+						}else {
+							sender.sendMessage(ChatUtils.phReplace(Vars.check_vl, p.getName(), num,
+									ConfigManager.instance.getPlayers()
+											.getInt("Punishments." + num + ".Level." + p.getUniqueId())));
 						}
 					}
-					sender.sendMessage(ChatUtils.colorize(ChatUtils.phReplace(Vars.no_type, args[0], args[1], 0)));
 					return true;
 				}
 				sender.sendMessage(ChatUtils.colorize(ChatUtils.phReplace(Vars.no_player, args[0], args[1], 0)));
@@ -161,4 +181,37 @@ public class Commands implements CommandExecutor {
 		}
 		return true;
 	}
+
+	public void logToFile(String message) {
+
+		try {
+			File dataFolder = Main.instance.getDataFolder();
+
+			if (!dataFolder.exists()) {
+				dataFolder.mkdir();
+			}
+
+			File saveTo = new File(Main.instance.getDataFolder(), "Punishment.log");
+			if (!saveTo.exists()) {
+				saveTo.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(saveTo, true);
+
+			PrintWriter pw = new PrintWriter(fw);
+
+			pw.println(message);
+
+			pw.flush();
+
+			pw.close();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+
 }
