7256862329e7cabafee155df1bf06c3d453b9570
Sms_Gamer <creepercannyon@gmail.com>
2019-10-07 18:55:10 -0700

    Create MainCommand.java

---
+++ src/main/java/uwu/smsgamer/uwup/Commands/SubCommands/MainCommand.java
@@ -0,0 +1,27 @@
+package uwu.smsgamer.uwup.Commands.SubCommands;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import uwu.smsgamer.uwup.ConfigManager.ConfigManager;
+import uwu.smsgamer.uwup.Utils.ChatUtils;
+import uwu.smsgamer.uwup.UwUPunishments;
+import uwu.smsgamer.uwup.Vars.Vars;
+
+/**
+ * CheckVl command.
+ */
+public class MainCommand {
+    /**
+     * Executes the CheckViolation command.
+     *
+     * @param sender Source of the command
+     * @param cmd    Command which was executed
+     * @param label  Alias of the command which was used
+     * @param args   Passed command arguments
+     */
+    public static void mainCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    }
+}
